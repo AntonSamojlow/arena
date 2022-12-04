4334a99a678aaa31a7110bc9f8aa980204db8517
AntonSamojlow <anton.samojlow@gmail.com>
2022-12-04 15:34:13 +0100

    added base cmake project 'recorder'

---
+++ recorder/src/recorder.cpp
@@ -0,0 +1,13 @@
+﻿// recorder.cpp : Defines the entry point for the application.
+//
+
+#include "recorder.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "recorder start" << endl;
+	cout << "recorder end" << endl;
+	return 0;
+}
