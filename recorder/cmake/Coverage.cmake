0543830d8865f3c2493054cafd5debb4bf1e610f
AntonSamojlow <anton.samojlow@gmail.com>
2023-01-07 16:28:56 +0100

    add help output

--- recorder/cmake/Coverage.cmake
+++ recorder/cmake/Coverage.cmake
@@ -3,6 +3,7 @@ include_guard()
 # Enable coverage reporting for gcc/clang
 function(enable_coverage target_name)
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    message(STATUS "coverage enabled")
     target_compile_options(${target_name} INTERFACE --coverage -O0 -g)
     target_link_libraries(${target_name} INTERFACE --coverage)
   else()
