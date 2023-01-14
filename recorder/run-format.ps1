04666def09e12e222d299d2bfb7d9710b91a9b23
AntonSamojlow <anton.samojlow@gmail.com>
2023-01-14 17:02:12 +0100

    adjusting clang format

--- recorder/run-format.ps1
+++ recorder/run-format.ps1
@@ -1,5 +1,7 @@
 Write-Host "formatting files - please check the git diff"
-# src
+# collect src files
 $files = Get-ChildItem -Path .\src -Include *.cpp, *.h, *.ixx -Recurse -File
+# collect src files
 $files += Get-ChildItem -Path .\test -Include *.cpp, *.h, *.ixx -Recurse -File
+# run clang-format
 $files | ForEach-Object {Write-Host "formatting: $_"; clang-format.exe -i $_}
