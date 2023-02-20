2fabb6122b762c7d504207a83275f11859783132
Anton Samojlow <Anton.Samojlow@web.de>
2023-02-20 21:46:00 +0100

    added unique resource with a test

--- recorder/src/sag/storage/UniqueResource.h
+++ recorder/src/sag/storage/UniqueResource.h
@@ -2,13 +2,14 @@
 #include <utility>
 
 namespace tools {
-
+/// wrapper around a resource, basically copied from:
+/// https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2020/n4873.html#scopeguard.uniqueres
+///
 /// unique_resource is a universal RAII wrapper for resource handles. Typically, such resource handles are of trivial
 /// type and come with a factory function and a clean-up or deleter function that do not throw exceptions. The clean-up
 /// function together with the result of the creation function is used to create a unique_resource variable, that on
 /// destruction will call the clean-up function. Access to the underlying resource handle is achieved through get() and
 /// in case of a pointer type resource through a set of convenience pointer operator functions
-/// --> reference: https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2020/n4873.html#scopeguard.uniqueres
 template <class R, class D>
 class unique_resource {
  private:
@@ -19,13 +20,17 @@ class unique_resource {
 
  public:
 	// constructors
-	unique_resource() = default;
+	unique_resource()
+		requires std::is_default_constructible_v<R> && std::is_default_constructible_v<D>
+			: resource_({}), deleter_({}), execute_on_reset_(false){};
+
 	template <class RR, class DD>
-		requires std::is_constructible_v<D, DD> &&
+		requires std::is_constructible_v<R1, RR> && std::is_constructible_v<D, DD> &&
 						 (std::is_nothrow_constructible_v<R1, RR> || std::is_constructible_v<R1, RR&>) &&
 						 (std::is_nothrow_constructible_v<D, DD> || std::is_constructible_v<D, DD&>)
-
-	unique_resource(RR&& res, DD&& del) noexcept {
+	unique_resource(RR&& res, DD&& del) noexcept(
+		(std::is_nothrow_constructible_v<R1, RR> || std::is_constructible_v<R1, RR&>)&&(
+			std::is_nothrow_constructible_v<D, DD> || std::is_constructible_v<D, DD&>)) {
 		try {
 			if constexpr (std::is_nothrow_constructible_v<R1, RR>) {
 				resource_ = std::forward<RR>(res);
@@ -46,22 +51,95 @@ class unique_resource {
 			del(resource_);
 		}
 	}
-	unique_resource(unique_resource&& rhs) noexcept { --TODO-- }
+
+	unique_resource(unique_resource const&) = delete;
+
+	unique_resource(unique_resource&& rhs) noexcept(
+		std::is_nothrow_move_constructible_v<R1>&& std::is_nothrow_move_constructible_v<D>) {
+		// If initialization of resource throws an exception, rhs is left owning the resource and will free it in due time:
+		if constexpr (std::is_nothrow_move_constructible_v<R1>(rhs.resource_)) {
+			resource_ = std::move(rhs.resource_);
+		} else {
+			resource_ = rhs.resource_;
+		}
+		try {
+			if constexpr (std::is_nothrow_move_constructible_v<D>(rhs.resource_)) {
+				deleter_ = std::move(rhs.deleter_);
+			} else {
+				deleter_ = rhs.deleter_;
+			}
+		} catch (...) {
+			rhs.deleter_(resource_);
+			rhs.release();
+		}
+		execute_on_reset_ = std::exchange(rhs.execute_on_reset_, false);
+	}
 
 	// destructor
-	~unique_resource();
+	~unique_resource() { reset(); }
 
-	// assignment
-	auto operator=(unique_resource&& rhs) noexcept -> unique_resource&;
+	// assignments
+	auto operator=(unique_resource const&) -> unique_resource& = delete;
+
+	auto operator=(unique_resource&& rhs) noexcept(
+		std::is_nothrow_move_assignable_v<R1>&& std::is_nothrow_move_assignable_v<D>) -> unique_resource& {
+		// If a copy of a member throws an exception, this mechanism leaves rhs intact and *this in the released state:
+		reset();
+		if constexpr (std::is_nothrow_move_assignable_v<R1>) {
+			if constexpr (std::is_nothrow_move_assignable_v<D>) {
+				resource_ = std::move(rhs.resource_);
+				deleter_ = std::move(rhs.deleter_);
+			} else {
+				deleter_ = rhs.deleter_;
+				resource_ = std::move(rhs.resource_);
+			}
+		} else {
+			if constexpr (std::is_nothrow_move_assignable_v<D>) {
+				resource_ = rhs.resource_;
+				deleter_ = std::move(rhs.deleter_);
+			} else {
+				resource_ = rhs.resource_;
+				deleter_ = rhs.deleter_;
+			}
+		}
+		execute_on_reset_ = std::exchange(rhs.execute_on_reset_, false);
+	}
 
 	// other member functions
-	auto reset() noexcept -> void;
+	auto reset() noexcept -> void {
+		if (execute_on_reset_) {
+			execute_on_reset_ = false;
+			deleter_(resource_);
+		}
+	}
+
 	template <class RR>
-	auto reset(RR&& new_value) -> void;
+	auto reset(RR&& new_value) -> void {
+		reset();
+		if constexpr (std::is_nothrow_assignable_v<R&, RR>) {
+			resource_ = std::forward<RR>(new_value);
+		} else {
+			resource_ = std::as_const(new_value);
+		}
+		execute_on_reset_ = true;
+	}
+
 	auto release() noexcept -> void { execute_on_reset_ = false; }
+
 	auto get() const noexcept -> const R& { return resource_; }
-	auto operator*() const noexcept { return *get(); }
-	auto operator->() const noexcept -> R { return resource_; }
+
+	auto operator*() const noexcept -> std::add_lvalue_reference_t<std::remove_pointer_t<R>>
+		requires std::is_pointer_v<R> && (not std::is_void_v<std::remove_pointer_t<R>>)
+	{
+		return *get();
+	}
+
+	auto operator->() const noexcept -> R
+		requires std::is_pointer_v<R>
+	{
+		return resource_;
+	}
+
 	auto get_deleter() const noexcept -> const D& { return deleter_; }
 };
 
@@ -69,49 +147,21 @@ class unique_resource {
 template <class R, class D>
 unique_resource(R, D) -> unique_resource<R, D>;
 
-// assignment
-template <class R, class D>
-auto unique_resource<R, D>::operator=(unique_resource<R, D>&& rhs) noexcept -> unique_resource<R, D>& {
-	reset();
-	if constexpr (std::is_nothrow_move_assignable_v<R1>) {
-		if constexpr (std::is_nothrow_move_assignable_v<D>) {
-			resource_ = std::move(rhs.resource_);
-			deleter_ = std::move(rhs.deleter_);
-		} else {
-			deleter_ = rhs.deleter_;
-			resource_ = std::move(rhs.resource_);
-		}
-	} else {
-		if constexpr (std::is_nothrow_move_assignable_v<D>) {
-			resource_ = rhs.resource_;
-			deleter_ = std::move(rhs.deleter_);
-		} else {
-			resource_ = rhs.resource_;
-			deleter_ = rhs.deleter_;
+/// This creation function exists to avoid calling a deleter function with an invalid argument.
+/// Any failure during construction of the return value will not call del(resource) if resource == invalid.
+/// Example: auto file = make_unique_resource_checked(
+///   ::fopen("potentially_nonexistent_file.txt", "r"), nullptr, [](auto fptr){ ::fclose(fptr); });
+template <class R, class D, class S = std::decay_t<R>>
+auto make_unique_resource_checked(R&& resource, const S& invalid, D&& del) noexcept(
+	std::is_nothrow_constructible_v<std::decay_t<R>, R>&& std::is_nothrow_constructible_v<std::decay_t<D>, D>)
+	-> unique_resource<std::decay_t<R>, std::decay_t<D>> {
+	try {
+		return {std::forward(resource), std::forward(del), resource != invalid};
+	} catch (...) {
+		if (resource != invalid) {
+			del(resource);
 		}
 	}
-	execute_on_reset_ = std::exchange(rhs.execute_on_reset_, false);
-}
-
-// other member functions
-template <class R, class D>
-auto unique_resource<R, D>::reset() noexcept -> void {
-	if (execute_on_reset_) {
-		execute_on_reset_ = false;
-		deleter(resource_);
-	}
-}
-
-template <class R, class D>
-template <class RR>
-auto unique_resource<R, D>::reset(RR&& new_value) -> void {
-	reset();
-	if constexpr (std::is_nothrow_assignable_v<R&, RR>) {
-		resource_ = std::forward<RR>(new_value);
-	} else {
-		resource_ = as_const(new_value);
-	}
-	execute_on_reset_ = true;
 }
 
 }  // namespace tools
---
+++ recorder/test/unique_resource_test.cpp
@@ -0,0 +1,48 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "sag/storage/UniqueResource.h"
+
+TEST_CASE("UniqueResourceTest", "[unique_resource]") {
+	struct WasteBin {
+		int count = 0;
+		auto increment() -> void { count++; }
+	};
+
+	struct TestClass {
+		bool alive = true;
+	};
+
+	struct Deleter {
+		WasteBin* bin = nullptr;
+		auto operator()(TestClass test_class) const {
+			test_class.alive = false;
+			bin->increment();
+		}
+	};
+
+	WasteBin waste_bin;
+	Deleter del{&waste_bin};
+	{
+		tools::unique_resource<TestClass, Deleter> resource{TestClass(), del};
+		CHECK(resource.get().alive);
+
+		TestClass dead_class = {false};
+		CHECK(!dead_class.alive);
+
+		// verify a value reset triggers a delete
+		CHECK(waste_bin.count == 0);
+		resource.reset(dead_class);
+		CHECK(waste_bin.count == 1);
+
+		// retrieve new object state
+		CHECK(!resource.get().alive);
+
+		// verify an empty reset triggers a delete but disablds 'execute_on_reset'
+		CHECK(waste_bin.count == 1);
+		resource.reset();
+		CHECK(waste_bin.count == 2);
+		resource.reset(TestClass{});
+		CHECK(waste_bin.count == 2);
+	}
+	CHECK(waste_bin.count == 3);
+}
