afff4ffa06d1f85a0d7555dc08a624555b403b85
Anton Samojlow <Anton.Samojlow@web.de>
2023-02-22 22:29:05 +0100

    imrpove nolint sections

--- recorder/src/sag/tools/SQLiteHandler.cpp
+++ recorder/src/sag/tools/SQLiteHandler.cpp
@@ -11,25 +11,23 @@
 
 #include "tools/SQLiteHandler.h"
 
-
-// NOLINTBEGIN
 namespace {
 
 /// callback that both reads rows and logs
-auto read_rows_callback(void* input_ptr, int argc, char** argv, char** azColName) -> int {
+auto read_rows_callback(void* input_ptr, int argc, char** argv, char** azColName) -> int {  // NOLINT (sqlite has C-API)
 	tools::ExecuteResult& result = *static_cast<tools::ExecuteResult*>(input_ptr);
 
 	if (result.header.empty()) {
 		result.header.reserve(static_cast<size_t>(argc));
 		for (int i = 0; i < argc; i++) {
-			result.header.emplace_back(azColName[i]);
+			result.header.emplace_back(azColName[i]);  // NOLINT (sqlite has C-API)
 		}
 	}
 
 	std::vector<std::string> row;
 	row.reserve(static_cast<size_t>(argc));
 	for (int i = 0; i < argc; i++) {
-		row.emplace_back(argv[i]);
+		row.emplace_back(argv[i]);  // NOLINT (sqlite has C-API)
 	}
 	result.rows.push_back(row);
 	return 0;
@@ -53,12 +51,12 @@ auto SQLiteHandler::initialize(std::string_view file_path, bool open_read_only) 
 	sqlite3* db_instance = nullptr;
 	auto result = sqlite3_open_v2(file_path.data(),
 		&db_instance,
-		open_read_only ? SQLITE_OPEN_READONLY : SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE,
+		open_read_only ? SQLITE_OPEN_READONLY : SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE,  // NOLINT (sqlite has C-API)
 		nullptr);
 	db_handle_.reset(db_instance);
 
 	if (result != SQLITE_OK) {
-		std::string error_msg = fmt::format("Failed to open database: '{}'", sqlite3_errstr(result));
+		std::string const error_msg = fmt::format("Failed to open database: '{}'", sqlite3_errstr(result));
 		logger_->error(error_msg);
 		throw std::exception(error_msg.c_str());
 	}
@@ -79,7 +77,5 @@ auto SQLiteHandler::execute(std::string_view statement) -> ExecuteResult {
 	logger_->debug("... read {} rows of {} columns", result.rows.size(), result.header.size());
 	return result;
 }
-
-// NOLINTEND
 
 }  // namespace tools
