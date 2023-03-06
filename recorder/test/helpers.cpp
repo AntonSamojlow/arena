665247634604f2f85eee18dd38b91f5abf29ecfa
AntonSamojlow <anton.samojlow@gmail.com>
2023-03-06 20:54:51 +0100

    misc fixes

--- recorder/test/helpers.cpp
+++ recorder/test/helpers.cpp
@@ -7,8 +7,8 @@
 namespace test {
 
 auto unique_file_path(bool create_file) -> TempFilePath {
-	std::string new_file = "test_tempfile_" + std::to_string((RNG::instance().nonnegative_number()));
-	std::filesystem::path path(new_file);
+	std::string const new_file = "test_tempfile_" + std::to_string((RNG::instance().nonnegative_number()));
+	std::filesystem::path const path(new_file);
 	if (std::filesystem::exists(path))
 		throw std::runtime_error("path '" + path.string() + "' already exists");
 
