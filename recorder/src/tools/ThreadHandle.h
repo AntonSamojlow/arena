d23fe06d9b0b35b77729019577c8a8f504d6cb49
AntonSamojlow <anton.samojlow@gmail.com>
2023-04-15 18:02:46 +0200

    adjust

--- recorder/src/tools/ThreadHandle.h
+++ recorder/src/tools/ThreadHandle.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <thread>
+
 #include "MutexQueue.h"
 #include "tools/MutexQueue.h"
 
--- recorder/test/sag/match_recorder_test.cpp
+++ recorder/test/sag/match_recorder_test.cpp
@@ -1,6 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
-#include <thread>
 
 #include "../helpers.h"
 #include "sag/TicTacToe.h"
